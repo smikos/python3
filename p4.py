904cc14a29eec3fde93e12b81b53f58fb4dc68c6
Konstantin Smirnov <sasharugby@yandex.ru>
2023-09-27 18:33:28 +0300

    goodDz

---
+++ p4.py
@@ -0,0 +1,19 @@
+#Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с повторениями)
+n1 = int(input("Количество 1:"))
+n2 = int(input("Количество 2:"))
+b1=(input("число 1: "))
+a1 = {b1,}
+for i in range(n1):
+    x=(input("число 1: "))
+    a1 = a1.union(x)
+    
+print(a1)
+b2=(input("число 2: "))
+a2 = {b2,}
+for i in range(n2):
+    x=(input("число 2: "))
+    a2 = a2.union(x)
+print(a2)
+
+i = a1.intersection(a2)
+print(i)
